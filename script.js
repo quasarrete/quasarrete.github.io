225617c3a6dbde5910550ba5e8106cfdad3f1e06
quasarrete <57726840+quasarrete@users.noreply.github.com>
2019-11-19 00:09:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var date1 = new Date();
+var date2 = new Date("12/08/2019");
+var date_diff_indays = function() {
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    diff =  Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+    return diff;
+    }
