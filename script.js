9d3017704a805e425bd26f745ecdf2f0682b23f1
Abdullah <35577822+hussa94@users.noreply.github.com>
2020-08-29 21:42:55 +0100

    Date format incorrect

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var date1 = new Date();
-var date2 = new Date("28/11/2020");
+var date2 = new Date("2020/11/28");
 var date_diff_indays = function() {
     dt1 = new Date(date1);
     dt2 = new Date(date2);
