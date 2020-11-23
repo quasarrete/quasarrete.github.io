6494b65b9292a4811b32730a67ddab439fc74efd
quasarrete <57726840+quasarrete@users.noreply.github.com>
2020-11-23 12:04:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var date2 = new Date("11/12/2020");
 var date_diff_indays = function() {
     dt1 = new Date(date1);
     dt2 = new Date(date2);
-    Console.Log(dt1);
-    Console.Log(dt2);
+    console.log(dt1);
+    console.log(dt2);
     diff =  Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
     if(diff<1){
     diff = "Wohoo"
