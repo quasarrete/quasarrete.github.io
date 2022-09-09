67b736c1d1d57bd0982c4d7883fe935e89b4fda5
Abdullah <husyn@gmail.com>
2022-09-09 23:12:33 +0100

    Updates

---
+++ precache-manifest.b616e0a948efee9347f897c94ed1cb37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff944b2b293240341c67",
+    "url": "/static/css/main.6c4d0bc0.chunk.css"
+  },
+  {
+    "revision": "ff944b2b293240341c67",
+    "url": "/static/js/main.ff944b2b.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "c45a8ed92f4a1622b906",
+    "url": "/static/js/2.c45a8ed9.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "0ff83cf590e8943eea4d979b8d7ba521",
+    "url": "/index.html"
+  }
+];
